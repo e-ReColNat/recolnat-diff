bbc47526f781465f6a9082db095b899f2051511f
webmastard <thomas.pateffoz@cirad.fr>
2016-05-13 10:57:01 +0200

    Suppression des champs created et modified du fait de la désynchro des bases de prod et de buffer

--- src/AppBundle/Entity/Bibliography.php
+++ src/AppBundle/Entity/Bibliography.php
@@ -77,16 +77,6 @@ class Bibliography
     private $type;
 
     /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $created;
-
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $modified;
-
-    /**
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Specimen", inversedBy="bibliographies", fetch="EXTRA_LAZY")
      * @ORM\JoinColumn(name="occurrenceid", referencedColumnName="occurrenceid")
      */
@@ -391,53 +381,6 @@ class Bibliography
         return $this->type;
     }
 
-    /**
-     * Set created
-     *
-     * @param \DateTime $created
-     *
-     * @return Specimen
-     */
-    public function setCreated($created)
-    {
-        $this->created = $created;
-
-        return $this;
-    }
-
-    /**
-     * Get created
-     *
-     * @return \DateTime
-     */
-    public function getCreated()
-    {
-        return $this->created;
-    }
-
-    /**
-     * Set modified
-     *
-     * @param \DateTime $modified
-     *
-     * @return Specimen
-     */
-    public function setModified($modified)
-    {
-        $this->modified = $modified;
-
-        return $this;
-    }
-
-    /**
-     * Get modified
-     *
-     * @return \DateTime
-     */
-    public function getModified()
-    {
-        return $this->modified;
-    }
 
     /**
      * Set specimen
--- src/AppBundle/Entity/Recolte.php
+++ src/AppBundle/Entity/Recolte.php
@@ -87,16 +87,6 @@ class Recolte
     private $verbatimeventdate;
 
     /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $created;
-
-    /**
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    private $modified;
-
-    /**
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Localisation", inversedBy="recoltes", fetch="EXTRA_LAZY")
      * @ORM\JoinColumn(name="locationid", referencedColumnName="locationid")
      */
@@ -453,55 +443,7 @@ class Recolte
         return $this->verbatimeventdate;
     }
 
-    /**
-     * Set created
-     *
-     * @param \DateTime $created
-     *
-     * @return Specimen
-     */
-    public function setCreated($created)
-    {
-        $this->created = $created;
-
-        return $this;
-    }
-
-    /**
-     * Get created
-     *
-     * @return \DateTime
-     */
-    public function getCreated()
-    {
-        return $this->created;
-    }
-
-    /**
-     * Set modified
-     *
-     * @param \DateTime $modified
-     *
-     * @return Specimen
-     */
-    public function setModified($modified)
-    {
-        $this->modified = $modified;
-
-        return $this;
-    }
-
-    /**
-     * Get modified
-     *
-     * @return \DateTime
-     */
-    public function getModified()
-    {
-        return $this->modified;
-    }
-
-    /**
+     /**
      * Set localisation
      *
      * @param Localisation|null $localisation
